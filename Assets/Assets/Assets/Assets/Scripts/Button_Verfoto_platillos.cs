d49528ec6a594cad02a51c591135002235730221
Medicen420 <50994647+medicen420@users.noreply.github.com>
2020-04-16 23:00:55 -0500

    Seguimos avanzando
    Ya he cambiado la resolucion en todas las pantallas, también he implementado más interacción dentro de la misma app, ya no quedo tan simple.

--- Assets/Assets/Assets/Assets/Scripts/Button_Verfoto_platillos.cs
+++ Assets/Assets/Assets/Assets/Scripts/Button_Verfoto_platillos.cs
@@ -12,6 +12,9 @@ public class Button_Verfoto_platillos : MonoBehaviour
     //estos son los botones pero se utilizan como gameobjects
     public GameObject btn_verfoticos;
     public GameObject btn_adiosfoticos;
+    //Este game object sera el efecto de sonido de una camara tomando una foto
+    //haciendo referencia a la fotografía que va a aparecer
+    public GameObject efecto_camara;
 
     
     //Esto es el animator que controla la animación de la fotografia del producto
@@ -26,6 +29,8 @@ public class Button_Verfoto_platillos : MonoBehaviour
         btn_verfoto.onClick.AddListener(PressButtonver);
         btn_adiosfoto.onClick.AddListener(Adiosbuttonver);
         script_buttoninfo = GetComponent<Button_info>();
+        //efecto_camara.SetActive(false);
+
 
     }
 
@@ -39,6 +44,7 @@ public class Button_Verfoto_platillos : MonoBehaviour
     {
         anim_foto.SetBool("Spa", true);
         anim_button.SetBool("ver", true);
+        efecto_camara.SetActive(true);
 
         //El problema era que estaba activando el boton que se esta presionando y eso no tenia logica,
         //ya que no puedes activar algo que ya esta activado, asi que cuando presionas el boton este se desactiva y se activa otro nuevo
@@ -48,6 +54,7 @@ public class Button_Verfoto_platillos : MonoBehaviour
         //Compartir el script fue la unica solucion para que se vuelva a activar la fotografia pero unicamente cuando presionamos el boton
         //de ver platillo o de visualizar la fotogreafía
         script_buttoninfo.btn_verfotografiaplatillo.SetActive(true);
+       
 
         
         Debug.Log("andamos al 100");
@@ -57,12 +64,14 @@ public class Button_Verfoto_platillos : MonoBehaviour
     {
         anim_foto.SetBool("Spa", false);
         anim_button.SetBool("ver", false);
+        efecto_camara.SetActive(false);
 
         //Aqui cuando presionamos por segunda vez el boton, este se desactiva y el primero se activa para que
         //se respete el ciclo que hay en la programación, podriamos decir que va de arriba hacia abajo y de abajo hacia arriba
         btn_verfoticos.SetActive(true);
         btn_adiosfoticos.SetActive(false);
         
+
 
     }
 }
